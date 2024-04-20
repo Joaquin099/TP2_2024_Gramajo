ea9c307a8dcebac9db0a9d8e99c4350cf013c0ca
Joaquin0999 <joaquin.gramajo.jg@gmail.com>
2024-04-20 16:10:23 -0300

    Subo codigo del tercer punto

--- src/main.c
+++ src/main.c
@@ -29,6 +29,9 @@ int main(void) {
 	// Habilitar el reloj para el puerto GPIOC (donde está conectado el pulsador B2)
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 
+	// Habilitar el reloj para el puerto GPIOB (donde está conectado el pulsador D14)
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
 	// Configurar el pin PA5 como salida
 	GPIO_InitTypeDef GPIO_InitStruct_LED;
 	GPIO_InitStruct_LED.GPIO_Pin = GPIO_Pin_5;
@@ -46,12 +49,20 @@ int main(void) {
 	GPIO_InitStruct_Pulsador.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOC, &GPIO_InitStruct_Pulsador);
 
+	//Configurar el pin PB9 como entrada (pulsador)
+	GPIO_InitTypeDef GPIO_InitStruct_Pulsador_Externo;
+	GPIO_InitStruct_Pulsador_Externo.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStruct_Pulsador_Externo.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStruct_Pulsador_Externo.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStruct_Pulsador_Externo.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOB, &GPIO_InitStruct_Pulsador_Externo);
+
 	while (1) {
 		// Leer el estado del pulsador
-		if (GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_13) == Bit_RESET) {
+		if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_9) == Bit_RESET || GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_13) == Bit_RESET) {
 			// El pulsador está presionado, encender el LED
 			GPIO_SetBits(GPIOA, GPIO_Pin_5);
-			} else {
+		} else {
 			// El pulsador no está presionado, apagar el LED
 			GPIO_ResetBits(GPIOA, GPIO_Pin_5);
 		}
