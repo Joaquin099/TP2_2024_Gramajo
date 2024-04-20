c5756ec86b013c99ffb51e1d547152fc2f2bab1c
Joaquin0999 <joaquin.gramajo.jg@gmail.com>
2024-04-20 17:34:46 -0300

    Modifique el codigo, resuelto por estados

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_gpio.h"
 
-// Prototipo de la función de inicialización del sistema
+// Prototipo de la funciï¿½n de inicializaciï¿½n del sistema
 void SystemInit(void);
 
 typedef enum {
@@ -20,17 +20,14 @@ typedef enum {
 } VALORES;
 
 int main(void) {
-	// Inicialización del sistema
+	// Inicializaciï¿½n del sistema
 	SystemInit();
 
 	// Habilitar el reloj para el puerto GPIOA
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
-	// Habilitar el reloj para el puerto GPIOC (donde está conectado el pulsador B2)
+	// Habilitar el reloj para el puerto GPIOC (donde estï¿½ conectado el pulsador B2)
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-
-	// Habilitar el reloj para el puerto GPIOB (donde está conectado el pulsador D14)
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 
 	// Configurar el pin PA5 como salida
 	GPIO_InitTypeDef GPIO_InitStruct_LED;
@@ -49,22 +46,30 @@ int main(void) {
 	GPIO_InitStruct_Pulsador.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOC, &GPIO_InitStruct_Pulsador);
 
-	//Configurar el pin PB9 como entrada (pulsador)
-	GPIO_InitTypeDef GPIO_InitStruct_Pulsador_Externo;
-	GPIO_InitStruct_Pulsador_Externo.GPIO_Pin = GPIO_Pin_9;
-	GPIO_InitStruct_Pulsador_Externo.GPIO_Mode = GPIO_Mode_IN;
-	GPIO_InitStruct_Pulsador_Externo.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStruct_Pulsador_Externo.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_Init(GPIOB, &GPIO_InitStruct_Pulsador_Externo);
+	VALORES VALOR = PRENDER;
 
 	while (1) {
-		// Leer el estado del pulsador
-		if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_9) == Bit_RESET || GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_13) == Bit_RESET) {
-			// El pulsador está presionado, encender el LED
-			GPIO_SetBits(GPIOA, GPIO_Pin_5);
-		} else {
-			// El pulsador no está presionado, apagar el LED
-			GPIO_ResetBits(GPIOA, GPIO_Pin_5);
+		switch(VALOR){
+		case PRENDER:
+			// Leer el estado del pulsador
+			if (GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_13) == Bit_RESET) {
+				// El pulsador estï¿½ presionado, encender el LED
+				GPIO_SetBits(GPIOA, GPIO_Pin_5);
+
+			}else{
+				VALOR = APAGAR;
+			}
+			break;
+
+		case APAGAR:
+			// El pulsador no estï¿½ presionado, apagar el LED
+			if (GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_13) != Bit_RESET){
+				GPIO_ResetBits(GPIOA, GPIO_Pin_5);
+
+			}else{
+				VALOR = PRENDER;
+			}
+			break;
 		}
 	}
 }
