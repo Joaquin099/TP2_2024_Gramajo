63da1705af1cc5ab0b03a62502840e88b5503065
Joaquin0999 <joaquin.gramajo.jg@gmail.com>
2024-04-20 17:12:51 -0300

    Configuracion para GPIO del led como salida GPIO_OType_OD

--- src/main.c
+++ src/main.c
@@ -42,7 +42,7 @@ int main(void) {
 	GPIO_InitStruct_LED.GPIO_Pin = GPIO_Pin_5;
 	GPIO_InitStruct_LED.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStruct_LED.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStruct_LED.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct_LED.GPIO_OType = GPIO_OType_OD;
 	GPIO_InitStruct_LED.GPIO_PuPd = GPIO_PuPd_NOPULL;
 
 	GPIO_Init(GPIOA, &GPIO_InitStruct_LED);
